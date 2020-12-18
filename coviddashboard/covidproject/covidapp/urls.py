afa5fc3c9ab023bb5c43d30ba0dfe3e0c3f461fa
Tom Lombaerts <tom.lombaerts@live.be>
2020-12-18 21:09:56 +0100

    urls

--- coviddashboard/covidproject/covidapp/urls.py
+++ coviddashboard/covidproject/covidapp/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('regions', views.regions, name='regions'),
 	path('prov', views.prov, name='prov'),
 	path('reg', views.reg, name='reg'),
-	path('provinceshtml', views.provinceshtml, name='provinceshtml')
 ]
